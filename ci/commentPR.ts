6071446765e830f25475fd3ac04c07d6056f428b
jakub.jozwiak <kubencki@gmail.com>
2021-04-15 00:12:31 +0200

    feat: test workflows

--- ci/commentPR.ts
+++ ci/commentPR.ts
@@ -4,7 +4,7 @@ export const commentPR = async ({ github, context }: { github: any; context: any
   let changelog = '';
   try {
     console.log(__dirname);
-    changelog = readFileSync('../CHANGELOG.md', 'utf8');
+    changelog = readFileSync('../../../CHANGELOG.md', 'utf8');
   } catch (error) {
     throw Error(`Missing changelog: ${error}`);
   }
