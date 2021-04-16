95725ba6ad684a7a557b75ed426d62c05cb6f866
jakub.jozwiak <kubencki@gmail.com>
2021-04-16 11:45:41 +0200

    feat: test workflows

--- ci/commentPR.ts
+++ ci/commentPR.ts
@@ -3,7 +3,7 @@ const { readFileSync } = require('fs');
 export const commentPR = async ({ github, context }: { github: any; context: any }) => {
   const changelog = readFileSync(`${__dirname}/../../CHANGELOG.md`, 'utf8');
 
-  const version = process.env.packageVersion;
+  const version = process.env.NPM_PACKAGE_VERSION;
   console.log('L:8 | VERSION: ', version);
 
   const newMessage = `
