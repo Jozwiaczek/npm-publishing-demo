8c0fd45c8a0fabcf9efdc3654efd0a8c7b5b759a
jakub.jozwiak <kubencki@gmail.com>
2021-04-27 17:14:33 +0200

    feat: test workflowsgit

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 console.log('Node build script');
-console.log(process.argv);
+const [nextVersion] = process.argv.slice(2);
+console.log(nextVersion);
 console.log(process.env.TMP);
 const changelog = fs.readFileSync(`./CHANGELOG.md`, 'utf8');
 console.log(changelog);
