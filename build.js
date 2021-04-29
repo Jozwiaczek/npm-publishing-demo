35ebbc613212dcb5d294d64b26b1ed1193310ae8
jakub.jozwiak <kubencki@gmail.com>
2021-04-29 12:13:27 +0200

    feat: asdasd - sad - asd
    [BREAKING CHANGE]

--- build.js
+++ build.js
@@ -5,3 +5,6 @@ console.log(nextVersion);
 console.log(process.env.TMP);
 const changelog = fs.readFileSync(`./CHANGELOG.md`, 'utf8');
 console.log(changelog);
+const currentRelease = changelog.split('# [')[1];
+const releaseNote = currentRelease.split('\n').slice(2).join('\n');
+console.log(releaseNote);
