dbbb1eb4073b4584428e66b302609a382d7950c3
jakub.jozwiak <kubencki@gmail.com>
2021-04-29 12:51:49 +0200

    fix: test changelog

--- release.config.js
+++ release.config.js
@@ -14,7 +14,7 @@ module.exports = {
     [
       '@semantic-release/exec',
       {
-        prepareCmd: 'yarn preCheckScript && yarn checkScript ${nextRelease}',
+        prepareCmd: 'yarn preCheckScript && yarn checkScript --json-object ${nextRelease}',
       },
     ],
     '@semantic-release/github',
