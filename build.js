0fd585ea589e11da763f1566ec8212d36698eeee
jakub.jozwiak <kubencki@gmail.com>
2021-04-29 12:47:00 +0200

    fix: test changelog

--- build.js
+++ build.js
@@ -2,7 +2,10 @@ console.log('\n\n---------------\n\n');
 console.log('Node build script');
 
 const [nextRelease] = process.argv.slice(2);
+console.log(nextRelease);
 const nextNotes = nextRelease.notes;
+console.log('L:6 | nextNotes: ', nextNotes);
+
 const releaseNote = nextNotes.split('\n').slice(2).join('\n');
 console.log('Release note:', releaseNote);
 console.log('\n\n---------------\n\n');
