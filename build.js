11698e0aed2187e5271dd871a9a82b64344aea67
jakub.jozwiak <kubencki@gmail.com>
2021-04-29 13:01:41 +0200

    fix: test changelog

--- build.js
+++ build.js
@@ -1,6 +1,8 @@
 console.log('\n\n---------------\n\n');
 console.log('Node build script');
 
+console.log('L:4 | process.argv: ', process.argv);
+
 const [nextRelease] = process.argv.slice(2);
 console.log(nextRelease);
 const nextNotes = nextRelease.notes;
